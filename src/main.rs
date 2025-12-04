55bfc52876dd2773e973ac4f2c8fae3973ba5190
Thierry Souche <thierry.souche@gmail.com>
2025-12-04 21:02:06 +0000

    Expanded to no-set-4 lists

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ use crate::n_list::*;
 
 fn main() {
     println!("Funny Set Exploration");
+    println!("======================");
 
-    let no_set_03_lists = n_list::create_all_03_no_set_lists();
+    // Create all seed lists (no-set-03 lists)
+    let no_set_03_lists: Vec<NList> = n_list::create_all_03_no_set_lists();
     println!("Created {} no-set-03 lists", no_set_03_lists.len());
 
-    let print_count = std::cmp::min(no_set_03_lists.len(), 12);
-    for i in 0..print_count {
-        let nlist = &no_set_03_lists[i];
-        println!("{}", nlist.to_string());
+    // expand to 4 cards no-set lists
+    let mut no_set_04_lists: Vec<NList> = Vec::new();
+    for list in &no_set_03_lists {
+        no_set_04_lists.extend(list.build_n_plus_1_no_set_lists());
     }
-    let no_set_03_list = no_set_03_lists[0].clone();
-    let no_set_04_lists = no_set_03_list.build_n_plus_1_no_set_lists();
     println!("From first no-set-03 list, created {} no-set-04 lists", no_set_04_lists.len());
 }
